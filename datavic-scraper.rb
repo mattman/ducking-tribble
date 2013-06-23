76c841e6ac0e1fde405fe71e245e1d489de2d18c
Matt Didcoe <matt@didcoe.id.au>
2013-06-23 14:51:58 +0800

    data.vic.gov.au scraper for Keith

---
+++ datavic-scraper.rb
@@ -0,0 +1,9 @@
+# data.vic.gov.au scraper
+# Author: Matt Didcoe <matt@mattdidcoe.com>
+# Last update: 23 June 2013
+
+# Usage: datavic-scraper query
+# Returns: CSV file of results
+
+require 'open-uri'
+require 'nokogiri'
